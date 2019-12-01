9a84f98438501b152be7ca46208fb96fffd02d4a
Damian Matyjaszek <tougepl@gmail.com>
2019-12-01 19:22:10 +0100

    Initial commit

---
+++ examples/BounceAnimation.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="sandbox">
+    <button class="sandbox__button" @click="show = !show">Show</button>
+    <transition name="bounce">
+      <img class="sandbox__image" v-show="show" src="./images/vuejs_logo.png" alt="vuejs">
+    </transition>
+  </div>
+</template>
+
+<script>
+  import './stylesheet/index.css'
+
+  export default {
+    name: 'BounceAnimation',
+    data () {
+      return {
+        show: false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  @keyframes bounce {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1)
+    }
+  }
+
+  .bounce-enter-active {
+    animation: bounce .5s;
+  }
+  .bounce-leave-active {
+    animation: bounce .5s reverse;
+  }
+</style>
---
+++ examples/CustomTransitionClasses.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sandbox">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <button class="sandbox__button" @click="show = !show">{{ show ? 'Hide' : 'Show' }}</button>
+    <transition name="rolling" enter-active-class="animated rollIn" leave-active-class="animated rollOut">
+      <img class="sandbox__image animated" v-show="show" src="./images/vuejs_logo.png" alt="vuejs">
+    </transition>
+  </div>
+</template>
+
+<script>
+  import './stylesheet/index.css'
+
+  export default {
+    name: 'CustomTransitionClasses',
+    data () {
+      return {
+        show: false
+      }
+    }
+  }
+</script>
